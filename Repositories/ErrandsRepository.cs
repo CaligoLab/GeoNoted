40e1e3c8d7089e29399480b29d4d87a2dd6a928d
Libret <70765177+Libret@users.noreply.github.com>
2022-11-08 21:47:55 +0200

    updated AddNewErrand() method
    Signed-off-by: Libret <70765177+Libret@users.noreply.github.com>

--- Repositories/ErrandsRepository.cs
+++ Repositories/ErrandsRepository.cs
@@ -2,10 +2,13 @@
 {
     public class ErrandsRepository
     {
-        public static void AddNewTopic(string errandName)
+        private static readonly string stringErrand = "Errand";
+        
+        public static void AddNewErrand(string errandName, string categorieId, string locationId, string placeId)
         {
             var id = Guid.NewGuid();
-            SQLTableManagement.InsertData("Errand", "ID, Name, Categorie, Location, Place", $"\" {id}\", \"{errandName}\"");
+            SQLTableManagement.InsertData("Errand", "ID, Name, CategorieId, LocationId, PlaceId", 
+                                          $"\" {id}\", \"{errandName}\", \"{categorieId}\", \"{locationId}\", \"{placeId}\"");
         }
 
     }
