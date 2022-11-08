b781ddc41829a6b6765da39bc0f51faa16a24864
Libret <70765177+Libret@users.noreply.github.com>
2022-11-08 21:06:07 +0200

    Update and rename TopicRepository.cs to ErrandsRepository.cs
    Signed-off-by: Libret <70765177+Libret@users.noreply.github.com>

---
+++ Repositories/ErrandsRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Geonote.Repositories
+{
+    public class ErrandsRepository
+    {
+        public static void AddNewTopic(string errandName)
+        {
+            var id = Guid.NewGuid();
+            SQLTableManagement.InsertData("Errand", "ID, Name, Categorie, Location, Place", $"\" {id}\", \"{errandName}\"");
+        }
+
+    }
+}
