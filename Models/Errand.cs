6b14c62748d63912d05445c550cf493ea70c6f69
Libret <70765177+Libret@users.noreply.github.com>
2022-11-08 21:33:07 +0200

    Update and rename Topic.cs to Errand.cs
    Signed-off-by: Libret <70765177+Libret@users.noreply.github.com>

---
+++ Models/Errand.cs
@@ -0,0 +1,20 @@
+﻿namespace Geonote.Models
+{
+    public class Errand
+    {
+        public string Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string CategorieId { get; set; }
+        
+        public string LocationId { get; set; }
+        
+        public string PlaceId { get; set; }
+
+        public Errand()
+        {
+
+        }
+    }
+}
