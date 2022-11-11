83c81415cc9c5db5d9fd99440844a57e3e793ef7
Libret <70765177+Libret@users.noreply.github.com>
2022-11-11 18:59:44 +0200

    Create Location.cs
    Signed-off-by: Libret <70765177+Libret@users.noreply.github.com>

---
+++ Models/Location.cs
@@ -0,0 +1,18 @@
+namespace Geonote.Models
+{
+    public class Location
+    {
+        public string Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Longitude { get; set; }
+        
+        public string Latitude { get; set; }
+
+        public Location()
+        {
+
+        }
+    }
+}
