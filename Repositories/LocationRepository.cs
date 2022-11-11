922b6ba610c5ee97b693609c804a66394826b062
Libret <70765177+Libret@users.noreply.github.com>
2022-11-11 19:07:56 +0200

    Create LocationRepository.cs
    Signed-off-by: Libret <70765177+Libret@users.noreply.github.com>

---
+++ Repositories/LocationRepository.cs
@@ -0,0 +1,15 @@
+namespace Geonote.Repositories
+{
+    public class LocationRepository
+    {
+        private static readonly string locationErrand = "Location";
+        
+        public static void AddNewErrand(string locationName, string longitude, string latitude)
+        {
+            var id = Guid.NewGuid();
+            SQLTableManagement.InsertData(locationErrand, "ID, Name, Longitude, Latitude", 
+                                          $"\" {id}\", \"{locationName}\", \"{longitude}\", \"{latitude}\"");
+        }
+
+    }
+}
