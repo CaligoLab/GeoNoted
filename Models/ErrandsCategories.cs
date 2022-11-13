dece451f47fba6384a5aba106adf466f35f2d699
tetianaraida <tetiana.raida@icloud.com>
2022-11-13 23:25:34 +0100

    add Categories to Models

---
+++ Models/ErrandsCategories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Geonote.Models
+{
+    public class ErrandsCategories
+    {
+        public string ID { get; set; }
+        public string name { get; set; }
+
+        public ErrandsCategories()
+        {
+        }
+    }
+}
+
