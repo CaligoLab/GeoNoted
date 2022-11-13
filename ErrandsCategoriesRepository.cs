ef4787ab8c0e388d126c8cb14d9056557d161a66
Tetiana <115114869+TetianaRaida@users.noreply.github.com>
2022-11-13 23:41:32 +0100

    Add files via upload
    Errands Categories file was  added
    
    Signed-off-by: Tetiana <115114869+TetianaRaida@users.noreply.github.com>

---
+++ ErrandsCategoriesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Geonote.Repositories
+{
+    public class ErrandsCategoriesRepository
+    {
+        private static readonly string ErrandCategory = "Category";
+
+        public static void AddNewErrandCategory(string categoryName)
+        {
+            var id = Guid.NewGuid();   
+            SQLTableManagement.InsertData(ErrandCategory, "ID, Name", $"\" {id}\", \"{categoryName}\"");
+        }
+        public ErrandsCategoriesRepository()
+        {
+        }
+    }
+}
+
