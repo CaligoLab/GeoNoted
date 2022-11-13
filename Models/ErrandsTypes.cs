2490beb36077b285a592199a1f2b5568709fcda2
Tetiana <115114869+TetianaRaida@users.noreply.github.com>
2022-11-13 23:48:01 +0100

    Add files via upload
    Categories and Types were added to Models. Types to discuss
    
    Signed-off-by: Tetiana <115114869+TetianaRaida@users.noreply.github.com>

---
+++ Models/ErrandsTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Geonote.Models
+{
+    public class ErrandsTypes
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public bool recurring { get; set; }
+        public bool justHint { get; set; }
+
+        public ErrandsTypes()
+        {
+        }
+    }
+}
