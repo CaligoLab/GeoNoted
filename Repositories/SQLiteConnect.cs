b3063d6a75177e6c1784b7308a4204e9d836f69f
Libret <70765177+Libret@users.noreply.github.com>
2022-11-15 21:25:02 +0200

    Update SQLiteConnect.cs
    Signed-off-by: Libret <70765177+Libret@users.noreply.github.com>

--- Repositories/SQLiteConnect.cs
+++ Repositories/SQLiteConnect.cs
@@ -5,18 +5,15 @@ namespace Geonote.Repositories
 {
     public class SQLiteConnect
     {
-        public SQLiteConnect()
+        private static SQLiteConnection _conn;
+
+
+        public static SQLiteConnection CreateConnection()
         {
 
-        }
-
-       public static SQLiteConnection CreateConnection()
-        {
-
-            // TT: Create a new database connection:
-            SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=database.db; Version = 3; New = True; Compress = True; ");
-         // TT: Open the connection:
-         try
+            SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=database.db; Version = 3; New = True; Compress = True;");
+            // Open the connection:
+            try
             {
                 sqlite_conn.Open();
             }
@@ -25,6 +22,25 @@ namespace Geonote.Repositories
 
             }
             return sqlite_conn;
+        }
+
+        public static SQLiteConnection GetSQLiteConnection()
+        {
+            if (_conn == null)
+            {
+                _conn = SqliteConnect.CreateConnection();
+            }
+            if (_conn.State == System.Data.ConnectionState.Closed)
+            {
+                _conn.Open();
+            }
+            return _conn;
+        }
+
+        public static void CoseConnections(SQLiteDataReader sqlite_datareader)
+        {
+            sqlite_datareader.Close();
+            GetSQLiteConnection().Close();
         }
     }
 }
