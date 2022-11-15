23ceb5e284a7a6b39d90e14f8c770f2108bbea30
Libret <70765177+Libret@users.noreply.github.com>
2022-11-15 20:36:05 +0100

    Create Place.cs

---
+++ Models/Place.cs
@@ -0,0 +1,14 @@
+﻿namespace Geonote.Models
+{
+    public class Place
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Place()
+        {
+
+        }
+    }
+}
