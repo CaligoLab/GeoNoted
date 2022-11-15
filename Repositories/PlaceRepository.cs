be762b57d81ddc243a2be473b8e10cf2b8e4284d
Libret <70765177+Libret@users.noreply.github.com>
2022-11-15 20:41:06 +0100

    Create PlaceRepository.cs

---
+++ Repositories/PlaceRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Geonote.Repositories
+{
+    public class PlaceRepository
+    {
+        private static readonly string stringPlace = "Place";
+
+        public static void AddNewErrand(string placeName)
+        {
+            var id = Guid.NewGuid();
+            SQLTableManagement.InsertData(stringPlace, "Id, Name",
+                                          $"\"{id}\", \"{placeName}\"");
+        }
+    }
+}
