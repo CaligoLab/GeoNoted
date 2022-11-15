8914e03a5262d4fa68dc0be08c6c7245f17b2cf6
Tetiana <115114869+TetianaRaida@users.noreply.github.com>
2022-11-15 23:24:52 +0100

    ErrandsTypes repository created
    A repository has been made for event types (with or without repeatability, just hints or full-fledged tasks). Now I made it as a blank for a table, but I'm not sure that this part of the logic should be implemented by creating a separate table for types. Maybe it should be a new class, in which there is a method with the behaviour logic for push notifications written through ʼbools' and 'ifs'?
    
    Signed-off-by: Tetiana <115114869+TetianaRaida@users.noreply.github.com>

---
+++ ErrandsTypesRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Geonote.Repositories
+{
+    public class ErrandsTypes
+    {
+        private static readonly string ErrandType = "Type";
+
+        public static void AddNewType(string typeName, bool recurring, bool justHint)
+        {
+            var id = Guid.NewGuid();
+            SQLTableManagement.InsertData(ErrandType, "ID, TypeName, Recurring, JustHint", $"\" {id}\", \"{typeName}\", \"{recurring}\", \"{justHint}\"");
+        }
+
+        public ErrandsTypes()
+        {
+
+        }
+    }
+}
+
