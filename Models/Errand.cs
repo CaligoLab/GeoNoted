cde5053475ac527cfbe83244df3df6d77bb0d9a2
Libret <70765177+Libret@users.noreply.github.com>
2022-11-16 09:41:31 +0100

    update errand repo

--- Models/Errand.cs
+++ Models/Errand.cs
@@ -1,4 +1,6 @@
-﻿namespace Geonote.Models
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Geonote.Models
 {
     public class Errand
     {
@@ -6,14 +8,16 @@
         
         public string Name { get; set; }
         
-        public ErrandsCategory Category { get; set; }
+        public List<ErrandsCategory> Categories { get; set; }
         
-        public Location Location { get; set; }
+        public List<Location> Locations { get; set; }
 
 
         public Errand()
         {
+            Categories = new List<ErrandsCategory>();
 
+            Locations = new List<Location>();
         }
     }
 }
--- Repositories/ErrandRepository.cs
+++ Repositories/ErrandRepository.cs
@@ -1,14 +1,16 @@
-﻿namespace Geonote.Repositories
+﻿using Geonote.Models;
+
+namespace Geonote.Repositories
 {
     public class ErrandRepository
     {
         private static readonly string stringErrand = "Errand";
         
-        public static void AddNewErrand(string errandName, string categorieId, string locationId, string placeId)
+        public static void AddNewErrand(string errandName, ErrandsCategory categorie, Location location)
         {
             var id = Guid.NewGuid();
-            SQLTableManagement.InsertData(stringErrand, "Id, Name, CategorieId, LocationId, PlaceId", 
-                                          $"\"{id}\", \"{errandName}\", \"{categorieId}\", \"{locationId}\", \"{placeId}\"");
+            SQLTableManagement.InsertData(stringErrand, "Id, Name, Category, LocationId, Location", 
+                                          $"\"{id}\", \"{errandName}\", \"{categorie}\", \"{location}\"");
         }
 
     }
