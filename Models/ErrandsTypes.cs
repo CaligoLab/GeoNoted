25b53aea7b9dab40078fd284cc5369afb50de429
tetianaraida <tetiana.raida@icloud.com>
2022-11-16 21:21:39 +0100

    AddNewErrandCategory method was added, typo in CloseConnections was corrected

---
+++ Models/ErrandsTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Geonote.Models
+{
+    public class ErrandsTypes
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Recurring { get; set; }
+
+        public bool JustHint { get; set; }
+
+        public ErrandsTypes()
+        {
+        }
+    }
+}
