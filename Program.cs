d56b797e183849a9c65574455642ae515671d680
Libret <70765177+Libret@users.noreply.github.com>
2022-11-17 12:47:12 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Geonote.Repositories;
 using System.Data.SQLite;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,9 +31,10 @@ var sqliteConnection = Geonote.Repositories.SQLiteConnect.CreateConnection();
 Geonote.Repositories.Migrations.run(sqliteConnection);
 
 /*
-Geonote.Repositories.TopicRepository.AddNewTopic("Grocery Stores");
-Geonote.Repositories.ItemRepository.AddNewItem("ICA Maxi", "Per Hallströms väg 15, 131 39 Nacka", "The biggest in the ICA chain, has the widest variety of items, including clothes, cutlery, and gardening/grilling supplies");
-Geonote.Repositories.LinkRepository.AddNewLink("ICA online shopping", "https://www.ica.se/handla/maxi-ica-stormarknad-koping-id_01014/", 1);
+
+
 */
+SQLTableManagement.CreateTable("CREATE TABLE Errand (Id VARCHAR(20), Name VARCHAR(200))");
+
 
 app.Run();
