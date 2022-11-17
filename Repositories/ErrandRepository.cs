56bfcbff9a72137364dea1d32eb56723babc21ee
Libret <70765177+Libret@users.noreply.github.com>
2022-11-17 16:03:03 +0100

    Update ErrandRepository.cs

--- Repositories/ErrandRepository.cs
+++ Repositories/ErrandRepository.cs
@@ -7,11 +7,11 @@ namespace Geonote.Repositories
     {
         private static readonly string stringErrand = "Errand";
         
-        public static void AddNewErrand(string errandName)
+        public static void AddNewErrand(string errandName, ErrandsCategory? category, Location? location)
         {
             var id = Guid.NewGuid();
-            SQLTableManagement.InsertData(stringErrand, "Id, Name", 
-                                          $"\"{id}\", \"{errandName}\"");
+            SQLTableManagement.InsertData(stringErrand, "Id, Name, CategoryId, LocationId", 
+                                          $"\"{id}\", \"{errandName}\", \"{category.Id}\", \"{location.Id}\"");
         }
 
     }
