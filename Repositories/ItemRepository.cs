e3b384a3427550aa1217e345e199c74ad622336b
Libret <70765177+Libret@users.noreply.github.com>
2022-11-17 12:15:23 +0100

    Update errand (added items list to errand) + ItemRepository

---
+++ Repositories/ItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using Geonote.Models;
+
+namespace Geonote.Repositories
+{
+    public class ItemRepository
+    {
+        private static readonly string stringItem = "Item";
+
+        public static void AddNewItem(string itemName)
+        {
+            var id = Guid.NewGuid();
+            SQLTableManagement.InsertData(stringItem, "Id, Name",
+                                          $"\"{id}\", \"{itemName}\"");
+        }
+    }
+}
