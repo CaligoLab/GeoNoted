5a2ce5d039a94cc439608ce5133ad804936606cf
tetianaraida <tetiana.raida@icloud.com>
2022-11-20 00:28:17 +0100

    added Hint to Models, Repo, migrations, database

---
+++ Models/Hint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Geonote.Models
+{
+    public class Hint
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Hint()
+        {
+        }
+    }
+}
+
