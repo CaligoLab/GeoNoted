81b5939152651882aa2f91bb64e32f9d4b57e050
Libret <70765177+Libret@users.noreply.github.com>
2022-11-28 09:40:04 +0100

    Updated db and Migrations

--- Repositories/Migrations.cs
+++ Repositories/Migrations.cs
@@ -12,7 +12,7 @@ namespace Geonote.Repositories
 
         public static void run(SQLiteConnection conn) // TA: Create a checkup if this method has been already run to avoid trying to make duplicates and crushing the app
         {
-            SQLTableManagement.CreateTable("CREATE TABLE Errand (Id VARCHAR(20), Name VARCHAR(200), Comment VARCHAR(200), CategoryId VARCHAR(20), LocationId VARCHAR(20))");
+            SQLTableManagement.CreateTable("CREATE TABLE Errand (Id VARCHAR(20), Name VARCHAR(200), Comment VARCHAR(200), CategoryId VARCHAR(20), LocationId VARCHAR(20), PlaceId VARCHAR(20))");
             SQLTableManagement.CreateTable("CREATE TABLE Category (Id VARCHAR(20), Name VARCHAR(200))");
             SQLTableManagement.CreateTable("CREATE TABLE Item (Id VARCHAR(20), Name VARCHAR(200), ErrandId VARCHAR(20))");
             SQLTableManagement.CreateTable("CREATE TABLE Location (Id VARCHAR(20), Longitude VARCHAR(200), Latitude VARCHAR(200), PlaceId VARCHAR(20))");
