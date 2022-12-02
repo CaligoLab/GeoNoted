999fb8b1c7a37ff0a5d36ee5dcfdbb3da961c4eb
SviatLama <ramanauskassvs@gmail.com>
2022-12-02 20:35:00 +0200

    Updated CategoryRepository

--- Repositories/CategoryRepository.cs
+++ Repositories/CategoryRepository.cs
@@ -1,15 +1,58 @@
-﻿using System;
+﻿using Geonote.Models;
+using System;
+using System.Data.SQLite;
+
 namespace Geonote.Repositories
 {
     public class CategoryRepository
     {
-        private static readonly string Category = "Category";
+        private static readonly string CategoryTableName = "Category";
 
         public static void AddNewCategory(string categoryName)
         {
             var id = Guid.NewGuid();   
-            SQLTableManagement.InsertData(Category, "Id, Name", $"\" {id}\", \"{categoryName}\"");
+            SQLTableManagement.InsertData(CategoryTableName, "Id, Name", $"\" {id}\", \"{categoryName}\"");
         }
+
+        public static List<Categorу> GetAllCategories()
+        {
+            SQLiteConnect.GetSQLiteConnection();
+            var allCategories = new List<Categorу>();
+            var sqlite_datareader = SQLTableManagement.ReadData(CategoryTableName, null);
+            while (sqlite_datareader.Read())
+            {
+                string categoryId = sqlite_datareader.GetString(0);
+                string categoryName = sqlite_datareader.GetString(1);
+                allCategories.Add(new Categorу
+                {
+                    Id = categoryId,
+                    Name = categoryName
+                });
+            }
+            SQLiteConnect.CloseConnections(sqlite_datareader);
+            return allCategories;
+        }
+
+
+        public static Categorу? GetCategory(string categoryId)
+        {
+            SQLiteConnect.GetSQLiteConnection();
+            var clause = $"Id = \"{categoryId}\"";
+            var sqlite_datareader = SQLTableManagement.ReadData(CategoryTableName, null);
+            while (sqlite_datareader.Read())
+            {
+                string categoryName = sqlite_datareader.GetString(1);
+                SQLiteConnect.CloseConnections(sqlite_datareader);
+                return new Categorу
+                {
+                    Id = categoryId,
+                    Name = categoryName
+                };
+            }
+            SQLiteConnect.CloseConnections(sqlite_datareader);
+            return null;
+        }
+
         public CategoryRepository()
         {
         }
