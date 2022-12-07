cf2cee126a23fc9cc4e662d4d36e9440b5527c65
CaligoLab <tanchikt3485@gmail.com>
2022-12-07 23:03:55 +0100

    merge

--- Repositories/Migrations.cs
+++ Repositories/Migrations.cs
@@ -17,10 +17,7 @@ namespace Geonote.Repositories
             SQLTableManagement.CreateTable("CREATE TABLE Item (Id VARCHAR(20), Name VARCHAR(200), ErrandId VARCHAR(20))");
             SQLTableManagement.CreateTable("CREATE TABLE Location (Id VARCHAR(20), Longitude VARCHAR(200), Latitude VARCHAR(200), PlaceId VARCHAR(20))");
             SQLTableManagement.CreateTable("CREATE TABLE Place (Id VARCHAR(20), Name VARCHAR(200), AddressId VARCHAR(300), Type VARCHAR(30), Comment VARCHAR(300), LocationId VARCHAR(20))");
-            SQLTableManagement.CreateTable("CREATE TABLE Type (Id VARCHAR(20), Name VARCHAR(200), Recurring INT, JustHint INT)"); //comment VARCHAR(200) ?
-            SQLTableManagement.CreateTable("CREATE TABLE Hint (Id VARCHAR(20), Name VARCHAR(200))");
             SQLTableManagement.CreateTable("CREATE TABLE Address (Id VARCHAR(20), Building VARCHAR(20), Street VARCHAR(200), Office VARCHAR(20), County VARCHAR(200), Country VARCHAR(200), Postcode VARCHAR(15), POBox VARCHAR(40), PlaceId VARCHAR(20), LocationId VARCHAR(20))");
-            SQLTableManagement.CreateTable("CREATE TABLE Place (Id VARCHAR(20), Name VARCHAR(200), AddressName VARCHAR(200))");
             SQLTableManagement.CreateTable("CREATE TABLE Type (Id VARCHAR(20), Name VARCHAR(200), Recurring INT, JustHint INT)"); //Comment VARCHAR(200) ?
             SQLTableManagement.CreateTable("CREATE TABLE Hint (Id VARCHAR(20), Name VARCHAR(200), CategoryId VARCHAR(20), LocationId VARCHAR(20))");
         }
