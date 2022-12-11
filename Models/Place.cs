8e19034e1b093f6593c9b343e80d28e287f33497
TA <caligolab@gmail.com>
2022-12-11 13:44:48 +0100

    Changed field naming to comply
    Signed-off-by: TA <caligolab@gmail.com>

--- Models/Place.cs
+++ Models/Place.cs
@@ -4,18 +4,18 @@
     {
         public string Id { get; set; }
 
-        public string placeName { get; set; }
-        public string? placeAddressId { get; set; }
-        public string? placeType { get; set; } // type or category? 
-        public string? placeComment { get; set; }
-        public string? placeLocationId { get; set; }
+        public string Name { get; set; }
+        public string? AddressId { get; set; }
+        public string? Category { get; set; } 
+        public string? Comment { get; set; }
+        public string? LocationId { get; set; }
 
-        public Place(string placeName, string? placeAddressId, string? placeType, string? placeComment, string? placeLocationId)
+        public Place(string placeName, string? placeAddressId, string? placeCategory, string? placeComment, string? placeLocationId)
         {
             Id = Guid.NewGuid().ToString();
             Name = placeName;
             AddressId = placeAddressId;
-            Type = placeType;
+            Category = placeCategory;
             Comment = placeComment;
             LocationId = placeLocationId;
 
