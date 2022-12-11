90c463aab8dee454e856488185fe396e51530c16
TA <caligolab@gmail.com>
2022-12-11 14:14:20 +0100

    corrected Place table columns names to comply
    Signed-off-by: TA <caligolab@gmail.com>

--- Repositories/Migrations.cs
+++ Repositories/Migrations.cs
@@ -16,7 +16,7 @@ namespace Geonote.Repositories
             SQLTableManagement.CreateTable("CREATE TABLE Category (Id VARCHAR(20) NOT NULL, Name VARCHAR(200) NOT NULL, PRIMARY KEY(Id))");
             SQLTableManagement.CreateTable("CREATE TABLE Item (Id VARCHAR(20) NOT NULL, Name VARCHAR(200) NOT NULL, ErrandId VARCHAR(20), PRIMARY KEY(Id))");
             SQLTableManagement.CreateTable("CREATE TABLE Location (Id VARCHAR(20) NOT NULL, Longitude VARCHAR(200) NOT NULL, Latitude VARCHAR(200) NOT NULL, PlaceId VARCHAR(20), PRIMARY KEY(Id))");
-            SQLTableManagement.CreateTable("CREATE TABLE Place (Id VARCHAR(20) NOT NULL, Name VARCHAR(200) NOT NULL, AddressId VARCHAR(300), Type VARCHAR(30), Comment VARCHAR(300), LocationId VARCHAR(20), PRIMARY KEY(Id))");
+            SQLTableManagement.CreateTable("CREATE TABLE Place (Id VARCHAR(20) NOT NULL, Name VARCHAR(200) NOT NULL, AddressId VARCHAR(300), Category VARCHAR(30), Comment VARCHAR(300), LocationId VARCHAR(20), PRIMARY KEY(Id))");
             SQLTableManagement.CreateTable("CREATE TABLE Type (Id VARCHAR(20) NOT NULL, Name VARCHAR(200) NOT NULL, Recurring INT, JustHint INT, PRIMARY KEY(Id))"); //Comment VARCHAR(200) ?
             SQLTableManagement.CreateTable("CREATE TABLE Hint (Id VARCHAR(20) NOT NULL, Name VARCHAR(200) NOT NULL, CategoryId VARCHAR(20), LocationId VARCHAR(20), PRIMARY KEY(Id))");
             SQLTableManagement.CreateTable("CREATE TABLE Address (Id VARCHAR(20) NOT NULL, Building VARCHAR(20), Street VARCHAR(200) NOT NULL, Office VARCHAR(20), County VARCHAR(200), Country VARCHAR(200), Postcode VARCHAR(15), POBox VARCHAR(40), PlaceId VARCHAR(20), LocationId VARCHAR(20), PRIMARY KEY(Id))");
