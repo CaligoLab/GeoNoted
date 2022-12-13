e68a9c55a8dbbb824b251680beaefd7a4f6f2d21
SviatlanaRamanauskas <ramanauskassvs@gmail.com>
2022-12-13 11:00:52 +0200

    CategoryRepository update/delete

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Geonote.Models;
 using Geonote.Repositories;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore.Sqlite;
+using System.Data.SQLite;
 using System.Runtime.ExceptionServices;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -33,32 +32,22 @@ app.MapControllerRoute(
 //Geonote.Repositories.Migrations.Run();
 //var sqliteConnection = Geonote.Repositories.SQLiteConnect.CreateConnection();
 //Geonote.Repositories.Migrations.run(sqliteConnection);
-/*<<<<<<< Updated upstream
-
-
-=======
 /*var errand = new Geonote.Models.Errand()
 {
     Name = "first",
     Comment = "smth"
 };
 Geonote.Repositories.ErrandRepository.AddNewErrand(errand);*/
-//>>>>>>> Stashed changes
+
 /*
 var allErrands = Geonote.Repositories.ErrandRepository.GetAllErrands();
 
 var errand_with_all_filds = Geonote.Repositories.ErrandRepository.GetErrandWithAllFields("b6621fa5-7b6b-47c7-bd60-339b245a792a");
 */
-
-///var icaMaxi = new Place("IcaMaxi");
+Geonote.Repositories.CategoryRepository.UpdateCategoryNameById("WomanBeauty", "b50f69b5-b878-485b-a794-2de48808123b");
 
 //Geonote.Repositories.CategoryRepository.AddNewCategory("AutoService");
 //Geonote.Repositories.CategoryRepository.AddNewCategory("Beauty");
-//Geonote.Repositories.PlaceRepository.AddNewPlace(icaMaxi);
-
-//var categories = Geonote.Repositories.CategoryRepository.GetAllCategories();
-//Console.WriteLine(categories);
-
 //var categoryById = Geonote.Repositories.CategoryRepository.GetCategory("2badb9c6-a0ad-40ac-8f17-8eeda659633f");
 //var getAllCategoties = Geonote.Repositories.CategoryRepository.GetAllCategories();
 //var getCategory = Geonote.Repositories.CategoryRepository.GetCategory(getAllCategoties[0].Id);
