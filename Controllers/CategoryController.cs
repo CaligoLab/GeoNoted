c750df891409082fa767cc4cdee1e64a3b87e122
SviatlanaRamanauskas <ramanauskassvs@gmail.com>
2022-12-17 19:11:40 +0200

    CategoryControllers/getAll

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Geonote.Models;
+using Geonote.Repositories;
+
+namespace Geonote.Controllers
+{
+    [Route("api/category")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public List<Categorу> GetAllCategories()
+        {
+            return CategoryRepository.GetAllCategories();
+        }
+
+
+    }
+}
