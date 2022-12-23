b33925720650ba79d635b9e6f7016c01590492ed
TA <caligolab@gmail.com>
2022-12-23 21:08:17 +0100

    comments in Models/Location.cs
    Discussion about the use of the fields
    
    Signed-off-by: TA <caligolab@gmail.com>

--- Models/Location.cs
+++ Models/Location.cs
@@ -8,7 +8,7 @@ namespace Geonote.Models
 
         public string Longitude { get; set; }
 
-        public Place Place { get; set; } //Mari: if we have locationId in Pace, do we also need place obj here?
+        public Place Place { get; set; } //Mari: if we have locationId in Pace, do we also need place obj here? //We actually need a List of Places here. Cause the same Address can have several Places sharing it (like a big mall with grocery shops and all kind of services etc)
 
         public Location()
         {
