a4c3c87e7a277985bbaae891413edb084afdf9a1
TA <caligolab@gmail.com>
2022-12-23 21:12:46 +0100

    comments in Models/Place
    Discussion about the usage of the fields
    
    Signed-off-by: TA <caligolab@gmail.com>

--- Models/Place.cs
+++ Models/Place.cs
@@ -8,7 +8,7 @@
         public string? AddressId { get; set; } //Mari: adressId here and placeId in Address class
         public string? Category { get; set; } 
         public string? Comment { get; set; }
-        public string? LocationId { get; set; } //Mari: do we need lication id in both address and place classes 
+        public string? LocationId { get; set; } //Mari: do we need lication id in both address and place classes //TA: I think we do as there's sometimes no "normal" Address assigned to the point on the Map we need. And the User might not fill in the Address manually (to be able to link the Address.LocationId) - IF I am thinking straight now.
 
        /* public Place(string placeName, string? placeAddressId, string? placeCategory, string? placeComment, string? placeLocationId)
         {
